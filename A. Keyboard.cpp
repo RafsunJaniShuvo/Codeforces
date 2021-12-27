38aa461c2aa9d3d948862feec658112196ff5268
RafsunJaniShuvo <81139556+RafsunJaniShuvo@users.noreply.github.com>
2021-12-27 23:02:22 +0600

    Add files via upload

---
+++ A. Keyboard.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+
+char keyboard[100] = "qwertyuiopasdfghjkl;zxcvbnm,./";
+
+int main()
+{
+    char way[2];
+    scanf("%s", way);
+
+    char input[120];
+    scanf("%s", input);
+
+    if(way[0] == 'L')
+    {
+        for(int i = 0; i < strlen(input); i++)
+        {
+            for(int j = 0; j < strlen(keyboard); j++)
+            {
+                if(input[i] == keyboard[j])
+                {
+                    printf("%c", keyboard[j + 1]);
+                    break;
+                }
+            }
+        }
+        printf("\n");
+    }
+    else
+    {
+        for(int i = 0; i < strlen(input); i++)
+        {
+            for(int j = 0; j < strlen(keyboard); j++)
+            {
+                if(input[i] == keyboard[j])
+                {
+                    printf("%c", keyboard[j - 1]);
+                    break;
+                }
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
